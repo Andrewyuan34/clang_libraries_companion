fce3164ded9cf40478dd9a8d9c696d65279658cb
Andrewyuan34 <1932595813@qq.com>
2024-08-21 22:37:03 -0700

    Remove unneccessary files

---
+++ slides/examples/ast_visitor_1/CMakeFiles/dummy.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/dummy.dir/data/hello.cpp.o"
+  "CMakeFiles/dummy.dir/data/hello.cpp.o.d"
+  "CMakeFiles/dummy.dir/data/simple_1.cpp.o"
+  "CMakeFiles/dummy.dir/data/simple_1.cpp.o.d"
+  "libdummy.a"
+  "libdummy.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/dummy.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
