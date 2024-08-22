3a22037fef3c5157ce2c7fdfbd34af8b3545c16c
Andrewyuan34 <1932595813@qq.com>
2024-08-21 22:34:34 -0700

    Add more example cases

--- slides/examples/command_line/main.cpp
+++ slides/examples/command_line/main.cpp
@@ -1,4 +1,4 @@
-#include <format>
+/*#include <format>
 #include <string>
 #include <clang/Tooling/CommonOptionsParser.h>
 #include <clang/Tooling/Tooling.h>
@@ -50,4 +50,82 @@ int main(int argc, const char **argv) {
 		llvm::outs() << std::format("    {}\n", path);
 	}
 	return 0;
+}*/
+
+#include <format>
+#include <string>
+#include <vector>
+#include <clang/Tooling/CommonOptionsParser.h>
+#include <clang/Tooling/Tooling.h>
+#include <llvm/Support/CommandLine.h>
+#include <llvm/Support/raw_ostream.h>
+
+using namespace std::literals;
+
+static llvm::cl::OptionCategory toolOptionCat("Tool Options");
+static llvm::cl::extrahelp
+  CommonHelp(clang::tooling::CommonOptionsParser::HelpMessage);
+static llvm::cl::extrahelp MoreHelp(
+	"This tool does not actually do anything useful.\n"
+	"Life is full of disappointments.  Get over it.\n"
+);
+
+// Original options
+llvm::cl::opt<std::string> outFile(
+  "o", llvm::cl::desc("Output file"), llvm::cl::value_desc("output_file"),
+  llvm::cl::cat(toolOptionCat));
+llvm::cl::opt<bool> verbose("verbose",
+  llvm::cl::desc("Enable verbose output."), llvm::cl::cat(toolOptionCat));
+llvm::cl::alias verbose2("v", llvm::cl::desc("Alias for -verbose"),
+  llvm::cl::aliasopt(verbose));
+llvm::cl::opt<bool> foobar("foobar",
+  llvm::cl::desc("Enable experimental features."), llvm::cl::Hidden);
+llvm::cl::opt<std::string> opName(llvm::cl::Positional, llvm::cl::Required, //Positional means that the option is a positional argument
+  llvm::cl::desc("Operation to perform."),
+  llvm::cl::value_desc("op_name"), llvm::cl::cat(toolOptionCat));
+
+// New options
+llvm::cl::opt<int> intOption("int-option",
+  llvm::cl::desc("Specify an integer value."), llvm::cl::value_desc("integer_value"),
+  llvm::cl::cat(toolOptionCat));
+llvm::cl::opt<std::string> strOption("str-option",
+  llvm::cl::desc("A single string option"),
+  llvm::cl::value_desc("string_value"), llvm::cl::cat(toolOptionCat));
+
+// Customized error handler
+static void handleError(const llvm::Twine &message) {
+    llvm::errs() << "Error: " << message << "\n";
+    llvm::errs() << "Try '--help' for more information.\n";
 }
+
+int main(int argc, const char **argv) {
+    llvm::Expected<clang::tooling::CommonOptionsParser> expectedOptionsParser(
+      clang::tooling::CommonOptionsParser::create(argc, argv, toolOptionCat));
+    if (!expectedOptionsParser) {
+        handleError(llvm::toString(std::move(expectedOptionsParser.takeError())));
+        return 1;
+    }
+    
+    clang::tooling::CommonOptionsParser& optionsParser = *expectedOptionsParser;
+
+    // Print the values of the options
+    llvm::outs()
+      << std::format("verbose: {}\n", static_cast<bool>(verbose))
+      << std::format("foobar: {}\n", static_cast<bool>(foobar))
+      << std::format("operation: {}\n",
+        !opName.empty() ? opName : "(null)"s)
+      << std::format("output file: {}\n",
+        !outFile.empty() ? outFile : "(null)"s)
+      << std::format("integer option: {}\n", intOption.getValue())
+      << std::format("string list: {}\n", strOption.empty() ? "none" : strOption.getValue());
+
+    llvm::outs() << std::format("number of compilation database entries: {}\n",
+      optionsParser.getCompilations().getAllCompileCommands().size());
+    llvm::outs() << "source paths:\n";
+    for (auto path : optionsParser.getSourcePathList()) {
+        llvm::outs() << std::format("    {}\n", path);
+    }
+
+    return 0;
+}
+
--- slides/examples/command_line_0/main.cpp
+++ slides/examples/command_line_0/main.cpp
@@ -1,3 +1,4 @@
+/*
 #include <format>
 #include <string>
 #include <string_view>
@@ -28,4 +29,74 @@ int main(int argc, char **argv) {
 	  << std::format("verbose: {}\n", static_cast<bool>(verbose));
 	for (auto i : values) {llvm::outs() << std::format("value: {}\n", i);}
 	for (auto i : files) {llvm::outs() << std::format("file: {}\n", i);}
+}
+*/
+#include <format>
+#include <string>
+#include <string_view>
+#include <llvm/Support/CommandLine.h>
+
+namespace lc = llvm::cl;
+
+static lc::OptionCategory toolOpts("Tool options");
+
+// Command line options
+// first argument represents the option name, which is used to access the value
+// second argument represents the number of arguments, which can be Optional, Required, or ZeroOrMore
+// third argument refers to the description of the option, which is displayed in the help message, desc means description
+static lc::opt<std::string> logFile("log-file", lc::ZeroOrMore, lc::desc("Specify log file"));
+static lc::opt<bool> verbose("verbose", lc::Optional, lc::desc("Enable verbose output"), lc::cat(toolOpts));
+static lc::opt<int> debugLevel("debug-level", lc::Optional, lc::init(-1), lc::desc("Set the debug level"), lc::cat(toolOpts));
+static lc::list<int> values("value", lc::ZeroOrMore, lc::desc("Specify values"), lc::cat(toolOpts));
+static lc::alias valuesAlias("v", lc::aliasopt(values), lc::desc("Alias for 'value'"));
+static lc::list<std::string> files(lc::Positional, lc::ZeroOrMore, lc::desc("Specify input files"), lc::cat(toolOpts));
+
+// Additional options
+static lc::opt<std::string> outputFile("output-file", lc::desc("Specify output file"), lc::cat(toolOpts));
+static lc::opt<int> count("count", lc::Optional, lc::init(1), lc::desc("Number of iterations"), lc::cat(toolOpts));
+static lc::opt<bool> flag("flag", lc::Optional, lc::desc("A boolean flag"), lc::cat(toolOpts));
+
+// Function to print the version
+static void printVersion(llvm::raw_ostream& out) {
+    out << "version 1.0.0\n";
+}
+
+// Function to handle errors
+static void handleError(const std::string& errorMsg) {
+    llvm::errs() << "Error: " << errorMsg << "\n";
+    llvm::errs() << "Use --help to see available options.\n";
+}
+
+int main(int argc, char **argv) {
+    // Set the version printer and parse command line options
+    lc::SetVersionPrinter(printVersion);
+    lc::ParseCommandLineOptions(argc, argv,
+      "This program illustrates the use of the LLVM CommandLine API.");
+
+    // Check for invalid options or missing required options
+    if (values.empty() && !files.empty()) {
+        handleError("At least one 'value' must be specified if files are provided.");
+        return 1;
+    }
+
+    // Print parsed options
+    llvm::outs()
+      << std::format("log file: {}\n", !logFile.empty() ? std::string_view(logFile) : "---")
+      << std::format("debug level: {}\n", debugLevel.getValue())
+      << std::format("verbose: {}\n", verbose ? "enabled" : "disabled")
+      << std::format("output file: {}\n", !outputFile.empty() ? std::string_view(outputFile) : "not specified")
+      << std::format("count: {}\n", count.getValue())
+      << std::format("flag: {}\n", flag ? "true" : "false");
+
+    // Print values
+    for (auto i : values) {
+        llvm::outs() << std::format("value: {}\n", i);
+    }
+
+    // Print files
+    for (auto& file : files) {
+        llvm::outs() << std::format("file: {}\n", file);
+    }
+
+    return 0;
 }
